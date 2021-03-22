d9a87493d4439e7ffa6f92ba41490446c518daf5
Fang He <hf.enigma@gmail.com>
2021-03-22 21:30:18 +0800

    fix linux-zen path comparision

--- .github/actions/makepkg/entrypoint.sh
+++ .github/actions/makepkg/entrypoint.sh
@@ -6,7 +6,7 @@ sed -i '/^CXXFLAGS=/ s/.*/CXXFLAGS="${CFLAGS}"/ p' /etc/makepkg.conf
 chown -R builder "$PWD"
 
 for d in */; do
-    if [[ -f "$d/PKGBUILD" && "$d" != "linux-zen" ]]; then
+    if [[ -f "$d/PKGBUILD" && "$d" != "linux-zen/" ]]; then
         pushd "$d"
         echo "Running makepkg with $PWD/PKGBUILD"
         sudo -u builder makepkg -fsCc --noconfirm
