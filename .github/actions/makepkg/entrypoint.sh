e7bddf7787b0a9d829c085d0ff83645f667b7f08
Fang He <hf.enigma@gmail.com>
2021-03-22 20:37:39 +0800

    add github actions

---
+++ .github/actions/makepkg/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sed -i '/^CFLAGS=/ s/.*/CFLAGS="-march='${INPUT_ARCH}' -O2 -pipe -fno-plt"/ p' /etc/makepkg.conf
+sed -i '/^CXXFLAGS=/ s/.*/CXXFLAGS="${CFLAGS}"/ p' /etc/makepkg.conf
+
+chown -R builder "$PWD"
+
+for d in */; do
+    if [[ -f "$d/PKGBUILD" && "$d" != "linux-zen" ]]; then
+        pushd "$d"
+        echo "Running makepkg with $PWD/PKGBUILD"
+        sudo -u builder makepkg -fsCc --noconfirm
+        popd
+    fi
+done
