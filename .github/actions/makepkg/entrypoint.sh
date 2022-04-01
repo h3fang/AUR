0a91bd55bcdf3434f672577e50cd17259d3162a3
Fang He <hf.enigma@gmail.com>
2022-04-01 23:48:03 +0800

    try to fix evince build error

--- .github/actions/makepkg/entrypoint.sh
+++ .github/actions/makepkg/entrypoint.sh
@@ -2,16 +2,16 @@
 
 set -eu
 
-sed -i '/^CFLAGS=/ s/-march=x86-64 -mtune=generic/-march='${INPUT_ARCH}'/' /etc/makepkg.conf
-sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
+sudo sed -i '/^CFLAGS=/ s/-march=x86-64 -mtune=generic/-march='${INPUT_ARCH}'/' /etc/makepkg.conf
+sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
 
-chown -R builder "$PWD"
+sudo chown -R builder "$PWD"
 
 for d in */; do
     if [[ -f "$d/PKGBUILD" && "$d" != "linux-zen/" ]]; then
         pushd "$d"
         echo "Running makepkg with $PWD/PKGBUILD"
-        sudo -u builder makepkg -fsCc --noconfirm
+        makepkg -fsCc --noconfirm
         popd
     fi
 done
