49c6b2dc19cb0d2044428261476f5b0cffec3263
Fang He <hf.enigma@gmail.com>
2022-04-02 00:05:43 +0800

    do not build evince

--- .github/actions/makepkg/entrypoint.sh
+++ .github/actions/makepkg/entrypoint.sh
@@ -8,7 +8,7 @@ sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
 chown -R builder .
 
 for d in */; do
-    if [[ -f "$d/PKGBUILD" && "$d" != "linux-zen/" ]]; then
+    if [[ -f "$d/PKGBUILD" && "$d" != "linux-zen/" && "$d" != "evince/" ]]; then
         pushd "$d"
         echo "Running makepkg with $PWD/PKGBUILD"
         sudo -H -u builder makepkg -fsCc --noconfirm
