8ff236a83b4bb85b20534360f5cc23c1987ab9bc
Fang He <hf.enigma@gmail.com>
2022-04-02 08:35:24 +0800

    clean files after makepkg

--- .github/actions/makepkg/entrypoint.sh
+++ .github/actions/makepkg/entrypoint.sh
@@ -6,12 +6,15 @@ sed -i '/^CFLAGS=/ s/-march=x86-64 -mtune=generic/-march='${INPUT_ARCH}'/' /etc/
 sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
 
 chown -R builder .
+mkdir pkgs
 
 for d in */; do
-    if [[ -f "$d/PKGBUILD" && "$d" != "linux-zen/" && "$d" != "evince/" ]]; then
+    if [[ -f "$d/PKGBUILD" && "$d" != "linux-zen/" ]]; then
         pushd "$d"
         echo "Running makepkg with $PWD/PKGBUILD"
-        sudo -H -u builder makepkg -fsCc --noconfirm
+        sudo -H -u builder makepkg -fsrCc --noconfirm
+        mv *.pkg.tar.zst ../pkgs/
+        rm -rf *
         popd
     fi
 done
