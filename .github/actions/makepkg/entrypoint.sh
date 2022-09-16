54392107a30e7cabfaf0ad21032002efcc7eef21
Fang He <hf.enigma@gmail.com>
2022-09-16 21:54:17 +0800

    apply shellcheck suggestions

--- .github/actions/makepkg/entrypoint.sh
+++ .github/actions/makepkg/entrypoint.sh
@@ -6,7 +6,7 @@ if [ -d "/github" ]; then
     sudo chown -R builder /github/workspace /github/home
 fi
 
-sudo sed -i '/^CFLAGS=/ s/-march=x86-64 -mtune=generic/-march='${INPUT_ARCH}'/' /etc/makepkg.conf
+sudo sed -i '/^CFLAGS=/ s/-march=x86-64 -mtune=generic/-march='"${INPUT_ARCH}"'/' /etc/makepkg.conf
 sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
 
 mkdir -p pkgs
@@ -14,4 +14,4 @@ mkdir -p pkgs
 cd "${INPUT_PKG}"
 echo "Running makepkg with $PWD/PKGBUILD"
 makepkg -fs --noconfirm
-mv *.pkg.tar.zst ../pkgs/
+mv -- *.pkg.tar.zst ../pkgs/
