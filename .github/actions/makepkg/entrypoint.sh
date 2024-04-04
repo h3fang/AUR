8e276b02756f0f15bda28d36c31ce1bba161ad69
Fang He <hf.enigma@gmail.com>
2024-04-04 15:05:41 +0800

    fix github actions

--- .github/actions/makepkg/entrypoint.sh
+++ .github/actions/makepkg/entrypoint.sh
@@ -2,13 +2,11 @@
 
 set -eu
 
-if [ -d "/github" ]; then
-    sudo chown -R builder /github/workspace /github/home
-fi
-
 sudo sed -i '/^CFLAGS=/ s/-march=x86-64 -mtune=generic/-march='"${INPUT_ARCH}"'/' /etc/makepkg.conf
+sudo sed -i '/^options=(/ s/\bdebug\b/!debug/' /etc/makepkg.conf
 sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
 
+sudo chown -R builder /github/workspace /github/home
 mkdir -p pkgs
 
 cd "${INPUT_PKG}"
